16549ab9253b0f49c7409b019959a12202bd2ff0
Vitaliy <vitaliykorzenkoua@gmail.com>
2024-08-08 00:11:44 +0300

    try fix
    try fix

--- back-end/app.js
+++ back-end/app.js
@@ -4,33 +4,37 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/users'); // users routers
 
 var app = express();
 
-// view engine setup
-
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/users', usersRouter);
+// Routes
+app.use('/users', usersRouter); // Использование маршрута users
+app.use('/', function (req, res) {
+    res.send('Hello World!');
+});
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
+    // Set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
+    // Send the error status
     res.status(err.status || 500);
+    res.json({ error: err.message });
 });
 
 module.exports = app;
